2c1ecca1ba32b90bf4b57d3ccc4f50833e96f2a5
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-01-31 00:21:23 +0300

    Add car client

--- Bridge_TemplateMethod/src/main/java/CarClient.java
+++ Bridge_TemplateMethod/src/main/java/CarClient.java
@@ -1,5 +1,20 @@
+import carbrands.Audi;
+import carbrands.BMW;
+import carbrands.Mercedes;
+import cartypes.Car;
+import cartypes.CrossoverCar;
+import cartypes.HatchbackCar;
+import cartypes.SportCar;
+
 public class CarClient {
     public static void main(String[] args) {
-        Ca
+        Car bmwCrossover = new CrossoverCar(new BMW());
+        bmwCrossover.run();
+
+        Car audiSportCar = new SportCar(new Audi());
+        audiSportCar.run();
+
+        Car mercedesHatchback = new HatchbackCar(new Mercedes());
+        mercedesHatchback.run();
     }
 }
