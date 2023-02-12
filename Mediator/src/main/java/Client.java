8d1a29a73021dc90d195d1e511f24e20a2f7129f
Gleb Golovnev <glebgolovnev2004@gmail.com>
2023-02-12 18:41:39 +0300

    Add Client

--- Mediator/src/main/java/Client.java
+++ Mediator/src/main/java/Client.java
@@ -1,5 +1,14 @@
+import mediator.IMediator;
+import mediator.Mediator;
+import mediator.Widgets;
+
 public class Client {
     public static void main(String[] args) {
+        IMediator mediator = new Mediator();
 
+        mediator.handleEvent(Widgets.FileList);
+        mediator.handleEvent(Widgets.DirList);
+        mediator.handleEvent(Widgets.SelectionEdit);
+        mediator.handleEvent(Widgets.FilterEdit);
     }
 }
